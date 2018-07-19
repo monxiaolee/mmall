9c5d4abe301b3fc2d6e7dac348c5ad0e92e04c6e
limengxiao <15893623902Lee>
2018-07-19 18:41:46 +0800

    增加登录业务逻辑判断

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -39,14 +39,43 @@ public class UserController {
         return response;
     }
 
-    @RequestMapping(value = "logout.do",method = RequestMethod.GET)
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> logout(HttpSession session){
-        System.out.println("limengxiao");
-        System.out.println(session);
-//        session.removeAttribute(Const.CURRENT_USER);
-//        return ServerResponse.createBySuccess();
-        return null;
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "register.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> register(User user) {
+        return iUserService.register(user);
+    }
+
+    public ServerResponse<String> checkValid(String str, String type) {
+        return iUserService.checkValid(str, type);
+    }
+
+    @RequestMapping(value = "get_user_info.do")
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session) {
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
+    }
+
+    @RequestMapping(value = "forget_check_answer.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetGetQuestion(String username) {
+        return iUserService.selectQuestion(username);
+    }
+
+    @RequestMapping(value = "forget_check_answer.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username, String question, String answer) {
+        return iUserService.checkAnswer(username, question, answer);
     }
 
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
@@ -8,6 +9,8 @@ import com.mmall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * Created by limengxiao on 2018/7/18.
@@ -37,6 +40,66 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public ServerResponse<String> register(User user) {
-        return null;
+        ServerResponse validResponse = this.checkValid(user.getUsername(), Const.USERNAME);
+        if(!validResponse.isSuccess()) {
+            return validResponse;
+        }
+        validResponse = this.checkValid(user.getEmail(), Const.EMAIL);
+        if(!validResponse.isSuccess()) {
+            return validResponse;
+        }
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+//        MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+        int resultCount = userMapper.insert(user);
+        if(resultCount == 0) {
+            return ServerResponse.createByErrorMessage("注册失败");
+        }
+        return ServerResponse.createBySuccessMessage("注册成功");
+    }
+
+    public ServerResponse<String> checkValid(String str, String type) {
+        if(org.apache.commons.lang3.StringUtils.isNotBlank(type)) {
+//            开始校验
+            if(Const.USERNAME.equals(type)) {
+                int resultCount = userMapper.checkUsername(str);
+                if(resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("用户名已存在");
+                }
+            }
+            if(Const.EMAIL.equals(type)) {
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("email已存在");
+                }
+            }
+        } else {
+            return ServerResponse.createByErrorMessage("参数错误");
+        }
+        return ServerResponse.createBySuccessMessage("校验成功");
+    }
+
+    @Override
+    public ServerResponse<String> selectQuestion(String username) {
+
+        ServerResponse validResponse = this.checkValid(username, Const.USERNAME);
+        if(validResponse.isSuccess()) {
+            return ServerResponse.createByErrorMessage("用户不存在");
+        }
+        String question = userMapper.selectQuestionByUsername(username);
+        if(org.apache.commons.lang3.StringUtils.isNotBlank(question)) {
+            return ServerResponse.createBySuccess(question);
+        }
+        return ServerResponse.createByErrorMessage("找回密码的问题是空的");
+    }
+
+    @Override
+    public ServerResponse<String> checkAnswer(String username, String question, String answer) {
+        int resultCount = userMapper.checkAnswer(username, question, answer);
+        if(resultCount > 0) {
+//            说明问题及问题答案是这个用户的并且是正确的
+            String forgetToken = UUID.randomUUID().toString();
+        }
+        return ServerResponse.createByErrorMessage("问题的答案错误");
     }
 }
