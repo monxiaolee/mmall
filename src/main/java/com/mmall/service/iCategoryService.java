4d31d5cd0b92942cdba21412855a23cdbdedf2e7
monxiaolee <li_meng_xiao@yeah.net>
2018-08-28 22:49:10 +0800

    增加文件

---
+++ src/main/java/com/mmall/service/iCategoryService.java
@@ -0,0 +1,16 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * Created by monxiaoLee on 2018/8/28.
+ */
+public class iCategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
+<head>
+    <meta charset="utf-8">
+</head>
 <body>
-<h2>mmall</h2>
+<h2>段能能测试</h2>
 </body>
 </html>
