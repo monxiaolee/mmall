5279c90a3fd9cb5bacfe9673b6c9e66fc757566f
monxiaolee <li_meng_xiao@yeah.net>
2018-09-19 00:33:58 +0800

    修改文件

--- src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mmall.service.impl;
 
 import com.mmall.common.ServerResponse;
+import com.mmall.dao.CategoryMapper;
 import com.mmall.pojo.Category;
 import com.mmall.service.ICategoryService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +18,26 @@ import java.util.List;
 public class CategoryServiceImpl implements ICategoryService {
     @Override
     public ServerResponse addCategory(String categoryName, Integer parentId) {
+
+        @Autowired
+        private CategoryMapper categoryMapper;
+
+        if(parentId == null || StringUtils.isBlank(categoryName)) {
+            return ServerResponse.createByErrorMessage("添加品类参数错误");
+        }
+
+        Category category = new Category();
+        category.setName(categoryName);
+        category.setParentId(parentId);
+        category.setStatus(true);//这个分类是可用的
+
+        int rowCount = categoryMapper.insert(category);
+
+        if(rowCount > 0) {
+            return ServerResponse.createBySuccessMessage("添加品类成功");
+        }
+        return ServerResponse.createByErrorMessage("添加品类失败");
+
         return null;
     }
 
